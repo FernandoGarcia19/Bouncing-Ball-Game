431b4b346542af4525313f0c5580860ea7233651
FernandoGarcia19 <fernando.garcia@ucb.edu.bo>
2024-05-11 03:19:50 -0400

    Implement Enemy class

--- Game/Enemy.hpp
+++ Game/Enemy.hpp
@@ -1,9 +1,28 @@
 #pragma once
 #include <SFML/Graphics.hpp>
+#include "Vector2D.hpp"
+#define ENEMY_LENGHT 100.0f
+#define ENEMY_HEIGHT 40.0f
 
 class Enemy {
 private:
-	sf::RectangleShape shape;
+	uint32_t m_id;
+	sf::RectangleShape m_shape;
+	Vector2D m_position;
+	bool active = true;
+
 public:
+	Enemy(uint32_t id, const Vector2D& pos)
+		: m_id(id), m_position(pos)
+	{
+		m_shape.setSize(sf::Vector2f(ENEMY_LENGHT, ENEMY_HEIGHT));
+		m_shape.setPosition(m_position.x(), m_position.y());
+	};
+	~Enemy() {}
+	void destroy() 
+	{
+		active = false;
+	}
+
 
 };
